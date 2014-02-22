ccb0db0ee18f8104dd19c8de00a7df43fa5364fd
Arlo Belshee <arlo.belshee@live.com>
2014-02-22 11:28:06 -0800

    Clean up the boilerplate a bit.

--- Player/Player/App.xaml.cs
+++ Player/Player/App.xaml.cs
@@ -1,109 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿// Presentation Karaoke Player
+// File: App.xaml.cs
+// 
+// Copyright 2014, Arlo Belshee. All rights reserved. See LICENSE.txt for usage.
+
+using System;
+using System.Diagnostics;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Globalization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+using JetBrains.Annotations;
 
 namespace Player
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
+	/// <summary>
+	///    Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class _App
+	{
+		/// <summary>
+		///    Initializes the singleton application object.  This is the first line of authored code
+		///    executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public _App()
+		{
+			InitializeComponent();
+			Suspending += _OnSuspending;
+		}
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+		/// <summary>
+		///    Invoked when the application is launched normally by the end user.  Other entry points
+		///    will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched([NotNull] LaunchActivatedEventArgs e)
+		{
+			_TurnOnDebuggingConsole();
+			var rootFrame = _CreateMainWindow(e);
+			_NavigateToCorrectStartingPage(e, rootFrame);
+			_ActivateApplication();
+		}
 
+		/// <summary>
+		///    Invoked when Navigation to a certain page fails
+		/// </summary>
+		/// <param name="sender">The Frame which failed navigation</param>
+		/// <param name="e">Details about the navigation failure</param>
+		private static void _OnNavigationFailed([NotNull] object sender, [NotNull] NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
+
+		/// <summary>
+		///    Invoked when application execution is being suspended.  Application state is saved
+		///    without knowing whether the application will be terminated or resumed with the contents
+		///    of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void _OnSuspending([NotNull] object sender, [NotNull] SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+			_StopBackgroundActivity();
+			_SaveApplicationState();
+			deferral.Complete();
+		}
+
+		[NotNull]
+		private static Frame _CreateMainWindow([NotNull] LaunchActivatedEventArgs e)
+		{
+			var rootFrame = Window.Current.Content as Frame;
+			if (rootFrame != null) return rootFrame;
+
+			rootFrame = _InitializeRootWindow(e);
+			Window.Current.Content = rootFrame;
+			return rootFrame;
+		}
+
+		[NotNull]
+		private static Frame _InitializeRootWindow([NotNull] LaunchActivatedEventArgs e)
+		{
+			var rootFrame = new Frame {Language = ApplicationLanguages.Languages[0]};
+			rootFrame.NavigationFailed += _OnNavigationFailed;
+
+			if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+			{
+				_LoadSavedStateFromTombstone();
+			}
+			return rootFrame;
+		}
+
+		private static void _ActivateApplication()
+		{
+			Window.Current.Activate();
+		}
+
+		private static void _NavigateToCorrectStartingPage([NotNull] LaunchActivatedEventArgs e, [NotNull] Frame rootFrame)
+		{
+			if (rootFrame.Content == null)
+			{
+				rootFrame.Navigate(typeof (MainPage), e.Arguments);
+			}
+		}
+
+		private static void _SaveApplicationState()
+		{
+		}
+
+		private static void _LoadSavedStateFromTombstone()
+		{
+		}
+
+		private static void _StopBackgroundActivity()
+		{
+		}
+
+		private void _TurnOnDebuggingConsole()
+		{
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
+			if (Debugger.IsAttached)
+			{
+				DebugSettings.EnableFrameRateCounter = true;
+			}
 #endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-                // Set the default language
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
-        }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
-    }
+		}
+	}
 }
--- Player/Player/MainPage.xaml.cs
+++ Player/Player/MainPage.xaml.cs
@@ -1,30 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿// Presentation Karaoke Player
+// File: MainPage.xaml.cs
+// 
+// Copyright 2014, Arlo Belshee. All rights reserved. See LICENSE.txt for usage.
 
 namespace Player
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	///    An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
 }
