8e7c028adb1c69e8a6d69a7d36046af30ca6c70b
Arlo Belshee <arlo.belshee@live.com>
2018-08-09 14:00:01 -0700

    B   Commit a build script, rather than the full deck. Because it is huge and binary.

--- build_presentation.ps1
+++ build_presentation.ps1
@@ -1,3 +1,3 @@
-rm -f .\Presentation.karaoke
+rm -Force .\Presentation.karaoke
 compress-archive -path .\Presentation\*.* -DestinationPath Presentation.karaoke.zip -CompressionLevel Optimal
 mv .\Presentation.karaoke.zip .\Presentation.karaoke
